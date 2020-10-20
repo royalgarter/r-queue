454af779b46fbea99d913b9cd8ff19ee6afb71a6
royalgarter@gmail.com <royalgarter@gmail.com>
2020-10-20 10:58:48 +0700

    init 1.0

---
+++ task.js
@@ -0,0 +1,106 @@
+const async = require('async');
+const md5 = require('crypto').createHash('md5');
+
+const T = { PREFIX: process.env.TASK_QUEUE_PREFIX || 'TSKQ' };
+
+const json = o => JSON.stringify(o);
+const ujoin = (...args) => args.join('_').toUpperCase();
+const sub = (type, name) => ujoin(T.PREFIX, type, name);
+const gid = (str, salt) => ujoin(T.PREFIX, 'ID', md5.update(str+(salt||'')).digest('hex'));
+const enqueue = (queues) => Array.isArray(queues) ? {queues, isArray: true} : {queues: [queues], isArray: false};
+const wrap = o => {
+	if (typeof o != 'object'||o._tid||o._json) throw new TypeError('Task must be non-circular Object without (_tid, _json) key');
+	return (o._tid = gid(json(o), Date.now())) && (o._json = json(o)) && o;
+}
+
+T.push = (cli, qs, tsk, cb) => {
+	let {queues, isArray} = enqueue(qs);
+	tsk = wrap(tsk);
+
+	async.map(queues, (q, next) => async.parallel([
+		next => cli.set(tsk._tid, tsk._json, next),
+		next => cli.lpush(sub('wait', q), tsk._tid, next),
+	], next), (e, r) => cb(e, tsk._tid, isArray ? r : r[0])); 
+}
+
+T.pull = (cli, q, cb) => {
+	async.waterfall([
+		next => cli.rpoplpush(sub('wait', q), sub('work', q), next),
+		(tid, next) => cli.get(tid, (e, r) => next(e, r, tid))
+	], cb); 
+}
+
+T.lpull = (cli, q, otps={times: 30, interval: 1e3}, cb) => {
+	async.retry(otps, next => T.pull(cli, q, (e,r) => next(!r ? (e || 'Q_EMPTY') : null, r) ), cb);
+}
+
+T.fpull = (cli, q, ms=1e3, cb) => {
+	let res = null;
+	async.forever(next => T.pull(cli, q, (e,r) => r ? next(r) : setTimeout(next, ms)), r => cb(null, r));
+}
+
+T.len = (cli, qs, cb) => {
+	let {queues, isArray} = enqueue(qs);
+
+	async.map(queues, 
+		(q, next) => async.map([
+			sub('wait', q), 
+			sub('work', q)
+		], (sub,next) => cli.llen(sub, next), next), 
+	(e, r) => cb(e, isArray ? r : r[0])) 
+}
+
+T.del = (cli, qs, tid, cb) => {
+	let {queues, isArray} = enqueue(qs);
+
+	async.map(queues, (q, next) => async.parallel([
+		next => cli.del(tid, next),
+		next => cli.lrem(sub('wait', q), 1, tid, next),
+		next => cli.lrem(sub('work', q), 1, tid, next),
+	], next), (e, r) => cb(e, isArray ? r : r[0]));
+}
+
+T.reset = (cli, qs, tid, cb) => {
+	let {queues, isArray} = enqueue(qs);
+
+	async.map(queues, (q, next) => async.parallel([
+		next => cli.lrem(sub('wait', q), 1, tid, next),
+		next => cli.lrem(sub('work', q), 1, tid, next),
+		next => cli.lpush(sub('wait', q), tid, next),
+	], next), (e, r) => cb(e, isArray ? r : r[0]));
+}
+
+T.flush = (cli, qs, cb) => {
+	let {queues, isArray} = enqueue(qs);
+	
+	async.map(queues, (q, next) => async.parallel([
+		next => cli.del(sub('wait', q), next),
+		next => cli.del(sub('work', q), next),
+	], next), (e, r) => cb(e, isArray ? r : r[0]));
+}
+
+T.wipe = (cli, wildcard, cb) => {
+	cli.eval(`'local keys = redis.call("keys", ARGV[1]); return #keys > 0 and redis.call("del", unpack(keys)) or 0' 0 prefix:*${T.PREFIX}*${wildcard}`)
+}
+
+module.exports = exports = T;
+
+if (process.env.TEST_TASK) {
+	const __config = require('../config');
+	const cli = require(__config.baseDir + 'services/redis').redisMain;
+	const q = 'TESTQ'
+
+	async.waterfall([
+		next => T.push(cli, q, {data: 1}, (e, r) => console.log('push', e, r) & next(e,r)),
+		(id, next) => T.len(cli, q, (e, r) => console.log('len', e, r) & next(e,id)),
+		// (id, next) => T.pull(cli, q, (e, r) => console.log('pull', e, r) &  next(e,id)),
+		(id, next) => T.fpull(cli, q, 1e3, (e, r) => console.log('fpull', e, r) &  next(e,id)),
+		// (id, next) => T.lpull(cli, q, {times: 30, interval: 200}, (e, r) => console.log('lpull', e, r) &  next(e,id)),
+		(id, next) => T.len(cli, q, (e, r) => console.log('len', e, r) & next(e,id)),
+		(id, next) => T.reset(cli, q, id, (e, r) => console.log('reset', e, r) & next(e,id)),
+		(id, next) => T.len(cli, q, (e, r) => console.log('len', e, r) & next(e,id)),
+		// (id, next) => T.del(cli, q, id, (e, r) => console.log('del', e, r) & next(e,id)),
+		(id, next) => T.flush(cli, q, (e, r) => console.log('flush', e, r) & next(e,id)),
+		(id, next) => T.len(cli, q, (e, r) => console.log('len', e, r) & next(e,id)),
+	])
+}
